b41cfe213b3639c1dffa592a2b13c45cf9afbec4
Amy Carney <click2carney@yahoo.com>
2018-08-31 06:39:29 -0800

    update gitignore and project name

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='maasapi',
+    name='marsweather',
     version='0.1.0',
     description='MAAS API',
     long_description=readme,
